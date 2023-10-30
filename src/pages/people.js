836311a6e718a1eb035f8ab00d3b504ef47c638b
suzannahurst <hurstsuzanna@gmail.com>
2023-10-30 18:15:09 +0000

    adjusting columns based on image size

--- src/pages/people.js
+++ src/pages/people.js
@@ -20,11 +20,52 @@ const CloudPageStyles = styled.div`
 `;
 
 const FlexContainerStyles = styled.div`
-  display: grid;
+  /* display: grid;
   grid-template-columns: repeat(10, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   grid-template-rows: auto;
   gap: 1.5rem;
-  height: 70vh;
+  height: 70vh; */
+  display: grid;
+
+  grid-auto-rows: 1fr; /* Set the height of each row to 1fr for equal distribution */
+  /* Adjust the gap as needed */
+  justify-content: space-evenly;
+  align-content: start; /* Adjust this as needed to control vertical alignment */
+  overflow: auto;
+  grid-auto-flow: dense; /* Controls how items are placed in the grid */
+  &.xlarge {
+    grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
+    grid-gap: 2rem;
+  }
+  s &.large {
+    grid-template-columns: repeat(auto-fill, minmax(15%, 1fr));
+    grid-gap: 2rem;
+  }
+  &.medium {
+    grid-template-columns: repeat(auto-fill, minmax(12%, 1fr));
+    grid-gap: 2rem;
+  }
+  &.med {
+    grid-template-columns: repeat(auto-fill, minmax(11%, 1fr));
+    grid-gap: 2rem;
+  }
+  &.small {
+    grid-template-columns: repeat(auto-fill, minmax(9%, 1fr));
+    grid-gap: 2rem;
+  }
+  &.xsmall {
+    grid-template-columns: repeat(auto-fill, minmax(8%, 1fr));
+    grid-gap: 2rem;
+  }
+  &.xxsmall {
+    grid-template-columns: repeat(auto-fill,minmax(7.5%,1fr));
+    grid-gap: 1rem;
+  }
+   &.xxxsmall {
+    grid-template-columns: repeat(auto-fill,minmax(7.5%,1fr));
+    grid-gap: 1rem;
+  }
 `;
 
 
@@ -37,8 +78,21 @@ const FlexItemStyles = styled.div`
   max-width: 100%;
   /* min-width: 100px; */
   box-sizing: border-box;
-  overflow: hidden;
+  /* overflow: hidden; */
+  object-fit: cover;
 `;
+
+// const FlexItemStyles = styled.div`
+  
+//   width: calc(100% / ${(props) => props.totalImages}*100);
+
+//   max-width: 100%;
+
+//   box-sizing: border-box;
+
+//   object-fit: cover;
+//   border: 3px solid white;
+// `;
 
 
 
@@ -70,10 +124,11 @@ const Person = ({ index, image, totalImages }) => {
   // console.log("totalImages", totalImages);
 
   return (
-    <FlexItemStyles className={`img-wrapper element-${index}`} 
-    // style={imgStyles}
-     totalImages={totalImages}
-     >
+    <FlexItemStyles
+      className={`img-wrapper element-${index}`}
+      // style={imgStyles}
+      totalImages={totalImages}
+    >
       <img className="img" src={image.asset.url} alt={`Image ${index}`} />
     </FlexItemStyles>
   );
@@ -81,7 +136,7 @@ const Person = ({ index, image, totalImages }) => {
 
 const CloudPage = () => {
   const [showPics, setShowPics] = useState(false);
-  // const [getPeople, { loading, error, data }] = useLazyQuery(GET_CLOUD_PEOPLE);
+//   // const [getPeople, { loading, error, data }] = useLazyQuery(GET_CLOUD_PEOPLE);
   const { loading, error, data } = useQuery(GET_PEOPLE_OPTIONS);
 
   if (loading) return <Loader />;
@@ -90,151 +145,49 @@ const CloudPage = () => {
 console.log(data)
   const images = data.options[0].images;
   console.log(images.length)
+// const images = ["test", "test", "test", "test", "test", "test", "test", "test", "test",];
 
+  let flexSize = "xlarge"
+if (images.length <= 12) {
+    flexSize = "xlarge"
+}else if (images.length >= 13 && images.length <= 24) {
+    flexSize = "large"
+  } else if (images.length >= 25 && images.length <= 35) {
+    flexSize = "medium";
+  }  else if (images.length >= 36 && images.length <= 48) {
+    flexSize = "med";
+  } else if (images.length >= 49 && images.length <= 70) {
+    flexSize = "small";
+  } else if (images.length >= 71 && images.length <= 88) {
+    flexSize = "xsmall";
+  } else if (images.length >= 89 && images.length <= 108) {
+    flexSize = "xxsmall";
+  } else if (images.length >= 109 && images.length <= 160) {
+    flexSize = "xxxsmall";
+  }
 
-  return (
-    <CloudPageStyles>
-      {showPics ? (
-        <FlexContainerStyles>
-          {images.map((image, index) => {
-            return (
-              <Person
-                key={index}
-                index={index}
-                image={image}
-                totalImages={images.length}
-              />
-            );
-          })}
-        </FlexContainerStyles>
-      ) : null}
-      <Footer showPics={showPics} setShowPics={setShowPics}/>
-    </CloudPageStyles>
-  );
+  console.log("flexSize", flexSize)
+    return (
+      <CloudPageStyles>
+        {showPics ? (
+          <FlexContainerStyles className={`${flexSize}`}>
+            {images.map((image, index) => {
+              return (
+                <Person
+                  key={index}
+                  index={index}
+                  image={image}
+                  totalImages={images.length}
+                />
+              );
+            })}
+          </FlexContainerStyles>
+        ) : null}
+        <Footer showPics={showPics} setShowPics={setShowPics} />
+      </CloudPageStyles>
+    );
 };
 
 
 export default CloudPage;
 
-
-// import { Link, graphql, useStaticQuery } from "gatsby";
-// import React, { useEffect, useState } from "react";
-// import styled from "styled-components";
-// import { devices } from "../styles/breakpoints.js";
-// import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image";
-
-// import { useQuery, useLazyQuery, gql } from "@apollo/client";
-// import { gsap } from "gsap";
-// import { CSSPlugin } from "gsap/CSSPlugin";
-// import Loader from "../components/Loader/index.js";
-// import GET_PEOPLE_OPTIONS from "../queries/GET_PEOPLE_OPTIONS.js";
-
-// const CloudPageStyles = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: flex-start;
-//   width: 100%;
-//   align-items: center;
-//   min-height: 90vh;
-//   .img-group-container {
-//     display: flex;
-//     flex-wrap: wrap;
-//     width: 100%;
-//     gap: 2rem;
-//     height: 100%;
-//     position: relative;
-//   }
-//   .img-wrapper {
-//     position: absolute;
-//   }
-//   .img {
-//     max-width: 100%;
-//   }
-//   .element {
-//     opacity: 0;
-//     transform: translateY(20px);
-//     transition:
-//       opacity 0.5s,
-//       transform 0.5s;
-//   }
-//   .animate {
-//     opacity: 1;
-//     transform: translateY(0);
-//   }
-// `;
-
-// const getRandomPosition = () => {
-//   const randomX = Math.floor(Math.random() * 90); // Random horizontal position (0-90%)
-//   const randomY = Math.floor(Math.random() * 90); // Random vertical position (0-90%)
-//   return { left: `${randomX}%`, top: `${randomY}%` };
-// };
-
-// const shuffleArray = (array) => {
-//   const shuffledArray = [...array];
-//   for (let i = shuffledArray.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-//   }
-//   return shuffledArray;
-// };
-
-// const Person = ({ index, image, totalImages }) => {
-//   useEffect(() => {
-//     gsap.registerPlugin(CSSPlugin);
-
-//     gsap.fromTo(
-//       `.element-${index}`,
-//       { opacity: 0, y: 20 },
-//       { opacity: 1, y: 0, duration: 0.5, delay: 0.2 * index },
-//     );
-//   }, [index]);
-
-//   const imgStyles = {
-//     width: `${300 / totalImages}%`,
-//     ...getRandomPosition(), // Apply random position to each image
-//   };
-
-//   return (
-//     <div className={`img-wrapper element-${index}`} style={imgStyles}>
-//       <img className="img" src={image.asset.url} alt={`Image ${index}`} />
-//     </div>
-//   );
-// };
-
-// const CloudPage = () => {
-//   const [showPics, setShowPics] = useState(false);
-//   const { loading, error, data } = useQuery(GET_PEOPLE_OPTIONS);
-
-//   if (loading) return <Loader />;
-//   if (error) return <p>Error: {JSON.stringify(error)}</p>;
-//   if (!data) return <p>Could not find data</p>;
-
-//   const images = data.options[0].images;
-//   const shuffledImages = shuffleArray(images);
-
-//   const clickHandler = () => {
-//     setShowPics(!showPics);
-//   };
-
-//   return (
-//     <CloudPageStyles>
-//       {showPics ? (
-//         <div className="img-group-container">
-//           {shuffledImages.map((image, index) => {
-//             return (
-//               <Person
-//                 key={index}
-//                 index={index}
-//                 image={image}
-//                 totalImages={shuffledImages.length}
-//               />
-//             );
-//           })}
-//         </div>
-//       ) : null}
-//       <button onClick={clickHandler}>Go</button>
-//     </CloudPageStyles>
-//   );
-// };
-
-// export default CloudPage;
